ab6ee82aaa3efa4a5ef3debbdda896a9b7777d50
Jromary <Yoreko*2>
2019-11-20 11:32:58 +0100

    oij

--- TP-LMC.pl
+++ TP-LMC.pl
@@ -1,7 +1,7 @@
 /* TP - Logique - Groupe : Julien Romary, Reynault Sies
 
-	Syntax:
-		consult('TP-LMC.pl').*/
+Syntax:
+consult('TP-LMC.pl').*/
 
 
 
@@ -40,17 +40,46 @@ regle( f(a) ?= f(b), check ).
 regle( f(a) ?= f(b), orient ).
 regle( f(a) ?= f(b), decompose ).
 regle( f(a) ?= f(b), clash ).*/
-
+/*
 regle(A ?= B, decompose).
-regle( f(A) ?= f(B), decompose ) :- regle( A ?= B  , decompose ).
-regle( f(A, X) ?= f(B, Y), decompose) :- regle(X ?= Y, decompose), regle(f(A) ?= f(B), decompose), echo(A).
+
+regle( f(A) ?= f(B), decompose ) :-
+    regle( A ?= B , decompose ).
+
+regle( f(A, X) ?= f(B, Y), decompose) :-
+    regle(X ?= Y, decompose), regle(f(A) ?= f(B), decompose), echo(A).
+*/
+/*
+p(x ?= x, decompose).
+p(f(x, A) ?= f(y, B), decompose) :- p(x ?= y, decompose).
+*/
+
+/*
+p(f([X|Y]), f(W|Z)) :- echo(X), echo(W).
+*/
+/*
+afficherattribut(Func1, Func2) :-
+    \+var(Func1),
+    \+var(Func2),
+    Func1 =.. [_ | Terms1],
+    Func2 =.. [_ | Terms2],
+    echo(bagof(Func1)),
+    echo(bagof(Func2)).
+*/
+
+afficherattribut(Func) :-
+    \+var(Func),
+    Func =.. [_ | Terms],
+    echo(Func).
 
 
+afficherParam([X|L]) :- echo(X), afficherParam(L), afficherattribut(X).
 
 
-
-
-
-
-
-
+unifie(Func1, Func2):-
+    \+var(Func1),
+    \+var(Func2),
+    Func1 =.. [X | Terms1],
+    Func2 =.. [Y | Terms2],
+    echo(X), echo(Terms1),
+    echo(Y), echo(Terms2).
